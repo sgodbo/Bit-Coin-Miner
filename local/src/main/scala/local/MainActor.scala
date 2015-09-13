37034c6f5f74ce8f2a895a1d12fa6ed8b25af078
varun vyas <moduvyas@gmail.com>
2015-09-13 11:08:18 -0400

    Remote host working

--- local/src/main/scala/local/MainActor.scala
+++ local/src/main/scala/local/MainActor.scala
@@ -9,11 +9,15 @@ import util.Random
 
 case class mining(k :Int , number :Int)
 
-
 case class startMiningLocally(k :Int)
 case class startMiningRemote(k :Int , ipAddress :String)
 case class receiveMinnedCoin(bitCoinMinned :ArrayBuffer[String])
 case class finishedMining(number :Int )
+case class setNumberOfZeros(numberOfZeros  :Int)
+case class gotWorkRequest()
+case class takeWork(numberOfZeros :Int, gatorID  :String, number :Int )
+case class receiveMinnedCoinFromRemote(bitCoinMinned :ArrayBuffer[String])
+
 
 class Worker extends Actor 
 {
@@ -21,7 +25,7 @@ class Worker extends Actor
 	{
 		case mining(k:	Int , number: Int)  =>
 		{
-			println("Mining starts ");
+			println("Mining starts " + number);
 			var bitcoins_mined :ArrayBuffer[String] =  ArrayBuffer[String]();
 			val deadline = 50.seconds.fromNow
 			var count = 0;
@@ -35,7 +39,7 @@ class Worker extends Actor
 				  {
 				  	//Bitcoin is found
 					bitcoins_mined += seed + "\t" + bitcoin
-					println(bitcoin)
+					//println(bitcoin)
 				  }
 				  if(count > 1000000)
 				  {
@@ -54,6 +58,8 @@ class Boss extends Actor
 	var total_bitCoinMinned :ArrayBuffer[String] = ArrayBuffer[String]()
 	var	TotalNumberOfWorker :Int = 0;
 	var sum :Int = 0;
+	val gatorID = "Moduvyas"
+	var numberOfZeros :Int = 0;
 	def receive =
 	{
 		case startMiningLocally(k :Int) =>
@@ -66,47 +72,69 @@ class Boss extends Actor
 					worker ! mining(k , i)
 					sum = sum + i;	 /*Race Condition?*/				
 				}
+				//sum = sum + 10
 			}
 			else
 			{
 		        val worker =context.actorOf(Props[Worker].withRouter(RoundRobinRouter(nrOfInstances= 12)))
-				for( i <- 1 to 5 )
+		        val numberOfActors = 5  /*Should change this variable to spawn number of actors*/
+				for( i <- 1 to numberOfActors )
 				{
+					println(" Calling ith worker " + i)
 					worker ! mining(k, i)
 					sum = sum + i;			/*Race Condition?*/				
 				}
+				//sum = sum + numberOfActors
 			}
 		}
 		case startMiningRemote(k :Int , ipAddress :String) =>
 		{
 			println("Starting remote workers")
+	        val numberOfActors = 5  /*Should change this variable to spawn number of actors*/
 	        //val worker=context.actorSelection("akka.tcp://Worker@"+ipAddress+":5152/user/Worker")
 	        //val remoteWorker = context.actorSelection("akka.tcp://RemoteActor@127.0.0.1:7890/user/Worker")
 	        //val remoteWorker = context.actorFor("akka://RemoteActor@"+ipAddress+":5150/user/Worker")
 			var remoteWorker = context.actorFor("akka.tcp://RemoteActor@127.0.0.1:7890/user/Worker")
-			for( i <- 1 to 5 )  /*  5 remote workers */
+			for( i <- 1 to numberOfActors )  /*  5 remote workers */
 				{
 					println(" Calling ith %d Time " + i)
 					remoteWorker !mining(k, i)
-					sum = sum + i;
+					 sum = sum + i;
 					//remoteWorker ! "This is message from server"
 				}
+			//sum = sum + numberOfActors
+		}
+		case gotWorkRequest() =>
+		{
+			println("Got work request from worker ")
+			sum = sum + 1; /* number of workers is increased */
+			sender !takeWork(numberOfZeros , gatorID , sum )
 		}
 		case receiveMinnedCoin(bitCoinMinned :ArrayBuffer[String]) =>
 		{
-			println("Adding to total")
+			//println("Adding to total")
 			total_bitCoinMinned ++= bitCoinMinned
+		}
+		case receiveMinnedCoinFromRemote(bitCoinMinned :ArrayBuffer[String]) =>
+		{
+			println("Received Minned Bit coins from worker " )
+			total_bitCoinMinned ++= bitCoinMinned  /*Race condition */
 		}
 		case finishedMining(number :Int ) =>
 		{
 			TotalNumberOfWorker = TotalNumberOfWorker + number
 
 			if(TotalNumberOfWorker == sum)
+			{
+				println("Number of Coins found " + total_bitCoinMinned.length )
 				total_bitCoinMinned.foreach( println )
+			}
 			else
 				println("TotalNumberOfWorker  " + TotalNumberOfWorker)
-
-
+		}
+		case setNumberOfZeros(zeros :Int) =>
+		{
+			numberOfZeros = zeros
 		}
 		case msg :String =>
 		{
@@ -121,6 +149,23 @@ object MainActor extends App
 	//println(args(0))
 	val system = ActorSystem("MainActor")
 	println(args(0))
+	val boss = system.actorOf(Props(new Boss), "Boss")
+	if(args.length == 1)
+	{
+		println("Local Mining")
+		boss ! setNumberOfZeros(args(0).toInt)
+		boss ! startMiningLocally(args(0).toInt)
+	}
+	/*Do we need to implement do nothing and waiting for remote worker */
+	//system.awaitTermination()
+	//stringGenerator ! argument
+
+}
+
+
+/*
+
+Dead code for making this mainActor as remote and local both
 	if( args.length == 2) /*Have to do error checking */
 	{
 		println("Other Server")
@@ -133,7 +178,4 @@ object MainActor extends App
 		val boss = system.actorOf(Props(new Boss), "Boss")
 		boss ! startMiningLocally(args(0).toInt)
 	}
-  system.awaitTermination()
-	//stringGenerator ! argument
-
-}
+*/
--- remote/src/main/scala/remote/RemoteActor.scala
+++ remote/src/main/scala/remote/RemoteActor.scala
@@ -1,4 +1,5 @@
 import akka.actor.{ActorSystem, ActorLogging, Actor, Props}
+import akka.routing.RoundRobinRouter
 
 import scala.concurrent.duration._
 import scala.collection.mutable.ArrayBuffer
@@ -6,7 +7,9 @@ import scala.collection.mutable.ArrayBuffer
 import java.security.MessageDigest
 import util.Random
 
-
+case class getWorkRequest(ipAddress :String)
+case class gotWorkRequest()
+case class takeWork(numberOfZeros :Int, gatorID  :String, number :Int )
 
 case class startMiningLocally(k :Int)
 case class startMiningRemote(k :Int , ipAddress :String)
@@ -14,12 +17,73 @@ case class receiveMinnedCoin(bitCoinMinned :ArrayBuffer[String])
 case class finishedMining(number :Int )
 case class mining(k: Int , number: Int)
 
+case class receiveMinnedCoinFromRemote(bitCoinMinned :ArrayBuffer[String])
 
 object RemoteActor extends App  {
-   val system = ActorSystem("RemoteActor")
-   val Worker = system.actorOf(Props[Worker], name = "Worker")
+	val system = ActorSystem("RemoteActor")
+	val RemoteBoss = system.actorOf(Props[RemoteBoss], name = "RemoteBoss")
+	println(args(0))
+   	RemoteBoss !getWorkRequest(args(0))
+
   // //Worker ! "Starting the remote worker Actor"
   // //Worker ! mining(5, 4)
+}
+
+class RemoteBoss extends Actor {
+	var total_bitCoinMinned :ArrayBuffer[String] = ArrayBuffer[String]()
+	var	TotalNumberOfWorker :Int = 0;
+	var sum :Int = 0;
+	var ipAddress :String = ""; 
+	var gotTheWork :Int = 0;
+	var number :Int = 0;
+	def receive =
+	{
+		case getWorkRequest(ipAddress :String) =>
+		{
+			println("Asking for work")
+			var remoteWorker = context.actorFor("akka.tcp://MainActor@"+ipAddress+":7890/user/Boss")
+			remoteWorker !gotWorkRequest()
+
+		}
+
+		case takeWork(numberOfZeros :Int ,  gatorID :String , number :Int) =>
+		{
+			println("Got work and starting local workers ")
+			gotTheWork = 1;
+			/*Currently gatorID is not used as it is hard coded in remote worker*/
+	        val worker = context.actorOf(Props[Worker].withRouter(RoundRobinRouter(nrOfInstances= 12)))
+	        val numberOfActors = 4  /*Should change this variable to spawn number of actors*/
+	        for( i <- 1 to numberOfZeros )
+	        {
+	        	worker !mining( numberOfZeros , i)
+	        	sum = sum + i;	        	
+	        }
+
+		}
+		case receiveMinnedCoin(bitCoinMinned :ArrayBuffer[String]) =>
+		{
+			//println("Adding to total")
+			total_bitCoinMinned ++= bitCoinMinned
+		}
+		case finishedMining(number_local :Int ) =>
+		{
+			println("Finnished in worker")
+			TotalNumberOfWorker = TotalNumberOfWorker + number_local
+			var remoteWorker = context.actorFor("akka.tcp://MainActor@"+ipAddress+":7890/user/Boss")
+			if(TotalNumberOfWorker == sum)
+			{
+				//total_bitCoinMinned.foreach( println )
+				println("Trying to send message to Boss")
+				remoteWorker !receiveMinnedCoinFromRemote(total_bitCoinMinned)
+				remoteWorker !finishedMining(number)  /*Have to check which function gets called */
+			}
+			else
+			{
+				println("TotalNumberOfWorker  on this remote worker" + TotalNumberOfWorker)
+				/*have to experiement with one val of worker variable of remote and calling again  */
+			}
+		}
+	}
 }
 
 class Worker extends Actor 
@@ -36,7 +100,7 @@ class Worker extends Actor
 		{
 			println("Mining starts on Remote Worker " + number );
 			var bitcoins_mined :ArrayBuffer[String] =  ArrayBuffer[String]();
-			val deadline = 20.seconds.fromNow
+			val deadline = 10.seconds.fromNow
 			var count = 0;
 			while(deadline.hasTimeLeft) {
 				/*  Reason for adding 'number' is, Different threads gets same random string as seed 
@@ -62,11 +126,11 @@ class Worker extends Actor
 				// }
 			}
 			sender ! receiveMinnedCoin(bitcoins_mined)
-			println("Number Of iteration " +count  + " For Minner\t" + number)
-			//sender ! receiveMinnedCoin(bitcoins_mined) // just incase count was not complete
+			println("Number Of iteration " +count  + " For Minner woker remote\t" + number)
 			sender ! finishedMining(number)
 			//context.stop(self) /*Don't want to stop Remote*/
-		}	
+		}
+	
 	} 
 }
 
